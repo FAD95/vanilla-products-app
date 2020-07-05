ecf609e1ed91024749b86dc0d89644d598d55c6f
Fadith Escorcia <feescorciam@protonmail.com>
2020-07-05 04:11:25 -0500

    No pude

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+const TerserJSPlugin = require('terser-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'src/App.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    chunkFilename: 'js/[id].[chunkHash].js',
+    filename: 'js/[name].js',
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css',
+      chunkFilename: 'css/[id].[hash].css',
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: ['**/app.*'],
+    }),
+  ],
+}
